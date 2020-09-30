186bcf233a9a65a9f79ffbf2a909e6ad47758bf3
weldermartins <weldermartins@gmail.com>
2020-09-30 14:11:02 -0300

    9.10. Usando two-way data binding

---
+++ meuprojeto/src/app.component.ts
@@ -0,0 +1,17 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nome = 'Welder';
+
+  adicionar(){
+    console.log(`Adicionando ${this.nome}`);
+  }
+     
+
+}
