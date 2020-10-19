aced8f58886d21869ed01d6b50fc47fbc8670dba
weldermartins <weldermartins@gmail.com>
2020-10-19 20:02:22 -0300

    9.17. Estilos CSS dinâmicos com ngStyle

---
+++ meuprojeto/src/app/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html',
+  styleUrls: ['./funcionario-form.component.css']
+})
+export class FuncionarioFormComponent  {
+
+  ultimoId = 0;
+  nome = 'Welder';
+  adicionado = false;
+  
+  @Output() funcionarioAdicionado = new EventEmitter();
+
+  adicionar(){    
+    this.adicionado =  true;
+    
+    const funcionario = {
+      id:  ++this.ultimoId,
+      nome: this.nome
+    };
+
+    this.funcionarioAdicionado.emit(funcionario);
+  }
+     
+
+ 
+
+}
